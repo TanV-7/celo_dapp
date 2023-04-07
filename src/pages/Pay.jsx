9b8e796a18dad09e8296695090da51ca2938a52b
TanV-7 <tanvipathare19@siesgst.ac.in>
2023-04-07 23:50:53 +0530

    ii

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -34,7 +34,7 @@ const Pay = () => {
       <div className="w-96 h-80 rounded-md flex items-center justify-center bg-gray-50">
         {qrData && <QRCode className="w-full h-full" value={qrData} />}
       </div>
-      <a href="https://google.com">
+      <a href="https://main--vermillion-rolypoly-847438.netlify.app/login">
       <button class="bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-purple-950 text-white font-bold py-2 px-4 border border-blue-700 rounded">
   LogOut
 </button>
